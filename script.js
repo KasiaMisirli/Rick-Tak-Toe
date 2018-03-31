d66e5279e5e809d2ec3dd661a4e9870540d7321d
kasia-misirli <kasia.misirli@gmail.com>
2018-03-31 17:18:52 +1100

    animation and sound effects

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-console.log("tic tac toe");
-
+//the grid that logs moves
 var grid = [
     ["1","2","3"],
     ["5","4","6"],
@@ -9,33 +8,30 @@ var grid = [
 var allKeys = document.querySelectorAll(".row div");
 var result;
 var cross =true;
-
+//switch player, updating grid and adding image into the grid
 var moves = function(event){
     if(event.target.className === "circle" || event.target.className === "ex" || result!== undefined){
         return;
     }else{
         if(cross==true && event.target.textContent != "X" && event.target.textContent != "0"){
-                event.target.classList.add("ex");
-                //event.target.textContent = "X";
-                var pos = event.target.id.split('');
-                grid[+pos[0]][+pos[1]] = "X";
+            event.target.classList.add("ex");
+            var pos = event.target.id.split('');
+            grid[+pos[0]][+pos[1]] = "X";
         }else if (cross!== true && event.target.textContent != "X" && event.target.textContent != "0"){
-                event.target.classList.add("circle");
-                //event.target.textContent = "0";
-                var pos2 =event.target.id.split('');
-                grid[+pos2[0]][+pos2[1]] = "0";
+            event.target.classList.add("circle");
+            var pos2 =event.target.id.split('');
+            grid[+pos2[0]][+pos2[1]] = "0";
         }   
         cross=!cross;
         checkWins();
-        
     }
 } 
-
+//listening for moves
 allKeys.forEach(function(item){
     item.addEventListener("click",moves);
 });
 
-
+//checking wins by checking the grid and adding animation for the winner
 var checkWins = function() {
     for(var i=0;i<grid.length;i++){
         for(var j=0;j<grid[i].length;j++){    
@@ -44,12 +40,12 @@ var checkWins = function() {
                 if(grid[i][2]==="X"){
                     result="<i>This is how we SURVIVE! </i>---Rick Grimes wins!";
                     rickWinner();
-                    document.querySelector(".zombie").audio;
+                    playAudioRick();
                     return (document.querySelector(".result").innerHTML=result);
                 }else if(grid[i][2]==="0"){
-                    result="<i>uuugh</i>---Zombelson Zombekowski wins";
+                    result="<i>uuugh</i>---Zombelson Zombekowski wins!";
                     zombieWinner();
-                    //song1();
+                    playAudioZombie();
                     return (document.querySelector(".result").innerHTML=result);
                 }
             }else if((grid[0][i]=== "X" && grid[1][i]==="X" && grid[2][i]==="X")||
@@ -57,11 +53,12 @@ var checkWins = function() {
                 if(grid[0][i]==="X"){
                     result="<i>This is how we SURVIVE! </i>---Rick Grimes wins!";
                     rickWinner();
+                    playAudioRick();
                     return (document.querySelector(".result").innerHTML=result);
                 }else if(grid[0][i]==="0"){
-                    result="<i>uuugh</i>---Zombelson Zombekowski wins";
+                    result="<i>uuugh</i>---Zombelson Zombekowski wins!";
                     zombieWinner();
-                    //song1();
+                    playAudioZombie();
                     return (document.querySelector(".result").innerHTML=result);
                 }
             }else if((grid[0][0]==="X" && grid[1][1]==="X" && grid[2][2]==="X")||
@@ -69,10 +66,12 @@ var checkWins = function() {
                 if(grid[0][0]==="X"){
                     result="<i>This is how we SURVIVE! </i>---Rick Grimes wins!";
                     rickWinner();
+                    playAudioRick();
                     return (document.querySelector(".result").innerHTML=result);
                 }else if(grid[0][0]==="0"){
-                    result="<i>uuugh</i>---Zombelson Zombekowski wins";
+                    result="<i>uuugh</i>---Zombelson Zombekowski wins!";
                     zombieWinner();
+                    playAudioZombie();
                     return (document.querySelector(".result").innerHTML=result);
                 }
             }else if((grid[2][0]==="X" && grid[1][1]==="X" && grid[0][2]==="X")||
@@ -80,10 +79,12 @@ var checkWins = function() {
                 if(grid[2][0]==="X"){
                     result="<i>This is how we SURVIVE! </i>---Rick Grimes wins!";
                     rickWinner();
+                    playAudioRick();
                     return (document.querySelector(".result").innerHTML=result);
                 }else if(grid[2][0]==="0"){
-                    result="<i>uuugh</i>---Zombelson Zombekowski wins";
+                    result="<i>uuugh</i>---Zombelson Zombekowski wins!";
                     zombieWinner();
+                    playAudioZombie();
                     return (document.querySelector(".result").innerHTML=result);
                 }
             }
@@ -91,54 +92,41 @@ var checkWins = function() {
     }
 }
 
-
+//When Rick is the winner, run below animation
 var rickWinner=function(){
     document.querySelector(".image2").animate([
-        // keyframes
         { transform: 'translateY(0px)' }, 
         { transform: 'translateY(-300px)' }
       ], { 
-        // timing options
         duration: 850,
-        iterations: 3
+        iterations: 2
       });
 }
-
-
+//when Zombie is the winner, run below animation
 var zombieWinner=function(){
     document.querySelector(".image1").animate([
-        // keyframes
         { transform: 'translateY(0px)' }, 
         { transform: 'translateY(-300px)' }
       ], { 
-        // timing options
         duration: 850,
-        iterations: 3
+        iterations: 2
       });
 }
 
+//sound effects
+var x = document.getElementById("song1"); 
 
-// var song1 = function(){ 
-//     var audio = document.getElementById('song1');
-//     audio.src = 
-//         'zombie/wav/' + 
-//         document.getElementById('song1').getAttribute('data-value');
-//     audio.load();
-//     audio.play();
-// }
-// song1();
+function playAudioZombie() { 
+    x.play(); 
+} 
+var y = document.getElementById("song2"); 
 
-// var target;
-// var song1= function(e) {
-//     //e.preventDefault();
-  
-//     var elm = e.target;
-//     var audio = document.getElementById('song1');
-  
-//     var source = document.getElementById('song1Source');
-//     source.src = elm.getAttribute('data-value');
-  
-//    // audio.load(); //call this to just preload the audio without playing
-//     audio.play(); //call this to play the song right away
-//   };
-//   song1();
+function playAudioRick() { 
+    y.play(); 
+} 
+var z = document.getElementById("song3"); 
+
+function playAudioOpening() { 
+    z.play(); 
+} 
+playAudioOpening();
